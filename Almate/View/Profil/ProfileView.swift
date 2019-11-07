4eb38de89b5c1ba61c2780951b43a4e80df85e8c
Mirza Fachreza <mirzaf99@gmail.com>
2019-11-07 19:00:11 +0700

    profile view work collectionview

--- Almate/View/Profil/ProfileView.swift
+++ Almate/View/Profil/ProfileView.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
-class ProfileView: UIView {
+class ProfileView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
 
     @IBOutlet weak var darkView: UIView!
+    @IBOutlet weak var workCollection: UICollectionView!
+    @IBOutlet weak var workNumber: UILabel!
+    var workAngka: Int = 5
+    override func awakeFromNib() {
+        
+        
+        workNumber.text = "(\(workAngka))"
+    workCollection.register(UINib(nibName: "ProfileCell", bundle: nil), forCellWithReuseIdentifier: "profileCell")
+        workCollection.dataSource = self
+        workCollection.delegate = self
+        
+    }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return workAngka
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "profileCell", for: indexPath as IndexPath) as! ProfileCell
+        return cell
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
