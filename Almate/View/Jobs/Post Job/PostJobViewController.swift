0183915584c3c663f4eaf01ae1e1dde0f91f2b35
Adrian <abiyoga98@gmail.com>
2019-11-11 15:53:25 +0700

    post job xib

---
+++ Almate/View/Jobs/Post Job/PostJobViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostJobViewController.swift
+//  Almate
+//
+//  Created by Adrian Suryo Abiyoga on 11/11/19.
+//  Copyright © 2019 Slamet Riyadi. All rights reserved.
+//
+
+import UIKit
+
+class PostJobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
