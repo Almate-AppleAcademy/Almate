7e2e549ac135fa2d91908933d38cd25b59218ddf
Eldrian <andieldrian@gmail.com>
2019-11-20 16:35:39 +0700

    Employer Profile Tampilan Done

---
+++ Almate/View/Profil/EMPLOYER PROFIL/EmployerView.swift
@@ -0,0 +1,21 @@
+//
+//  EmployerView.swift
+//  Almate
+//
+//  Created by Andi Ikhsan Eldrian on 20/11/19.
+//  Copyright © 2019 Slamet Riyadi. All rights reserved.
+//
+
+import UIKit
+
+class EmployerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
