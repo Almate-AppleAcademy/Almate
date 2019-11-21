44d0b7358e5794f4de40d87e7fa74fa809681a3b
Gabriella Gracia <gabriellagraciamt@icloud.com>
2019-11-21 16:50:16 +0700

    ini udh bisa di check skill tag nya

---
+++ Almate/View/Profil/EMPLOYER PROFIL/EmployerSkillCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EmployerSkillCollectionViewCell.swift
+//  Almate
+//
+//  Created by Gabriella Gracia MT on 21/11/19.
+//  Copyright © 2019 Slamet Riyadi. All rights reserved.
+//
+
+import UIKit
+
+class EmployerSkillCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var skillTagView: UICollectionView!
+    @IBOutlet weak var skillTagNem: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Almate/View/Profil/EMPLOYER PROFIL/FavoriteCollectionViewCell.swift
+++ Almate/View/Profil/EMPLOYER PROFIL/FavoriteCollectionViewCell.swift
@@ -11,9 +11,27 @@ import UIKit
 class FavoriteCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var peopleSavedOutlet: UIImageView!
+    @IBOutlet weak var skillTagCollectionView: UICollectionView!
+    
+    @IBOutlet var skillsTagnya: EmployerSkillCollectionViewCell!
+    
+    let skillTags = ["Swift", "Java", "C++", "Kotlin", "Ruby", "Sketch", "SAP HR"]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        skillTagCollectionView.register(UINib(nibName: "skillsTagnya", bundle: nil), forCellWithReuseIdentifier: "skillTagDiEmployer")
         // Initialization code
     }
+}
 
+extension FavoriteCollectionViewCell: UICollectionViewDataSource, UICollectionViewDelegate
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "skillTagDiEmployer", for: indexPath) as! FavoriteCollectionViewCell
+        return  cell
+    }
 }
